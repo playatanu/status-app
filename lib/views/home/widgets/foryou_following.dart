bcfa80779459ae37fa35c4ca67dfdd9ec2be060d
Atanu Debnath <playatanuyt@gmail.com>
2021-11-20 11:08:38 +0530

    add

--- lib/views/home/widgets/foryou_following.dart
+++ lib/views/home/widgets/foryou_following.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: avoid_print
 // ignore: must_be_immutable
+import 'package:mood/util/background_color_list.dart';
 import 'package:mood/util/config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mood/views/post/post_page.dart';
+import 'package:mood/views/post/widgets/post_color_palate.dart';
+import 'package:mood/views/post/widgets/post_done_button.dart';
+import 'package:mood/views/post/widgets/post_textfield.dart';
+import 'package:mood/views/post/widgets/post_textstyle_button.dart';
 
 import '../../../landing_page.dart';
 
@@ -41,12 +46,48 @@ class ForYouFollowing extends StatelessWidget {
           InkWell(
               onTap: () {
                 print('object');
-                Get.to(PostPageInit());
+                Get.bottomSheet(
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 1.1,
+                    child: Obx(
+                      () => Container(
+                          color: colors[postcontoller.background_color.value],
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 20),
+                            child:
+                                Stack(alignment: Alignment.center, children: [
+                              Center(
+                                  child: Padding(
+                                padding: const EdgeInsets.only(bottom: 100),
+                                child: PostTextEdit(
+                                  postcontoller: postcontoller,
+                                ),
+                              )),
+                              PostDoneButton(postcontoller: postcontoller),
+                              PostTextStyleButton(postcontoller: postcontoller),
+                              Positioned(
+                                  bottom: 20,
+                                  child: SizedBox(
+                                    height: 60,
+                                    width: 350,
+                                    child: Center(
+                                        child: ColorPalet(
+                                            postcontoller: postcontoller)),
+                                  )),
+                            ]),
+                          )),
+                    ),
+                  ),
+                  enableDrag: true,
+                  isScrollControlled: true,
+                  persistent: true,
+                );
               },
-              child: Icon(
+              child: const Icon(
                 Icons.post_add,
                 color: white,
-                size: 20,
+                size: 30,
               )),
         ],
       ),
