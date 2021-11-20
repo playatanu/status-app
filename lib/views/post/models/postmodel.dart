e067200f6e59ba5f25e8dd8f868aaab750baea6a
Atanu Debnath <playatanuyt@gmail.com>
2021-11-20 11:09:27 +0530

    delect

--- lib/views/post/models/postmodel.dart
+++
@@ -1,8 +0,0 @@
-class PostModel {
-  String? posttext;
-  int? colorindex;
-  int? stylrindex;
-  String? username;
-
-  PostModel({this.posttext, this.colorindex, this.stylrindex, this.username});
-}
